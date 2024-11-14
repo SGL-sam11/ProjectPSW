c0bdf36fde6638270afe9e6800011cd87809cd89
SGL-sam11 <hutagaolsamuel14@gmail.com>
2024-11-14 15:34:08 +0700

    membuat website kimia kelas 11

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+// import Hidrokarbon from './components/Hidrokarbon';
+// import termokimia from './components/termokimia';
+// import reaksi from './components/reaksi';
+// import kesetimabangan from './components/kesetimabangan';
+// import asambasa from './components/asambasa';
+// import hidrolosis from './components/hidrolosis';
+// import penyangga from './components/penyangga';
+// import kelarutan from './components/kelarutan';
+// import koloid from './components/koloid';
+
+
+
+
+
+function App() {
+  return (
+  );
+}
+
+export default App;
